19de8a5779ebc2de7e052119aacb6bc2151254c2
Adrián <adrian.garcia8t@gmail.com>
2023-09-20 02:34:01 +0200

    Paco is now resolved

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: adrian <adrian@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 21:29:33 by adrigarc          #+#    #+#             */
-/*   Updated: 2023/09/20 01:08:54 by adrian           ###   ########.fr       */
+/*   Updated: 2023/09/20 02:33:02 by adrian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,12 @@ char	*ft_free(char **str)
 	*str = NULL;
 	return (NULL);
 }
+/*
+[Description ft_free]
+	1- Free the str memory
+	2- Make Null the new value of str
+	3- Return NULL
+*/
 
 char	*ft_2free(char **str1, char **str2)
 {
@@ -27,11 +33,17 @@ char	*ft_2free(char **str1, char **str2)
 	*str2 = NULL;
 	return (NULL);
 }
+/*
+[Description ft_free]
+	1- Free the str1 & str2 memory
+	2- Make Null the new value of str & str2
+	3- Return NULL
+*/
 
 char	*ft_read(int fd, char *mBox)
 {
-	int			bytes;
-	char		*buffer;
+	int		bytes;
+	char	*buffer;
 
 	bytes = 1;
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
@@ -58,37 +70,36 @@ char	*ft_read(int fd, char *mBox)
 
 char	*ft_cut(char *mBox)
 {
-	int		count1;
-	int		count2;
-	char	*aux;
+	int		cont_a;
+	int		cont_b;
+	char	*temp;
 
-	count1 = 0;
-	count2 = 0;
+	cont_a = 0;
+	cont_b = 0;
 	if (!mBox)
 		return (NULL);
-	while (mBox[count1] && mBox[count1] != '\n')
-		count1++;
-	if (!mBox[count1])
+	while (mBox[cont_a] && mBox[cont_a] != '\n')
+		cont_a++;
+	if (!mBox[cont_a])
 		return (ft_free(&mBox));
-	if (mBox[count1] == '\n')
-		count1++;
-	aux = malloc(sizeof(char) * (ft_strlen(mBox) - count1 + 1));
-	if (!aux)
+	if (mBox[cont_a] == '\n')
+		cont_a++;
+	temp = malloc(sizeof(char) * (ft_strlen(mBox) - cont_a + 1));
+	if (!temp)
 		return (ft_free(&mBox));
-	while (mBox[count1])
-		aux[count2++] = mBox[count1++];
-	aux[count2] = '\0';
+	while (mBox[cont_a])
+		temp[cont_b++] = mBox[cont_a++];
+	temp[cont_b] = '\0';
 	free(mBox);
-	return (aux);
+	return (temp);
 }
 
 char	*get_next_line(int fd)
 {
-	static char		*box;
-	char			*line;
-	int				i;
+	static char	*box = NULL;
+	char		*line;
+	int			i;
 
-	box = NULL;
 	i = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
@@ -104,3 +115,47 @@ char	*get_next_line(int fd)
 	box = ft_cut(box);
 	return (line);
 }
+
+/*
+[Description get_next_line]
+	1-Check if the fd or the BUFFER_SIZE are <0
+		{If its}
+			a-Return NULL
+	2-Read the fd and put inside of the string static
+	3-Check if the static exist
+	4-Iterate while container have content and its not a \n
+	5-With the firts \n detection, put the rest inside a temp string
+	6-Cut the line from the static and put inside again
+	7- Check if line[0] exist
+		{If doesn't}
+			a-Free line and static
+	8- Return line
+
+[Description ft_read]
+	1- Create a mallow and check if its exist
+		{If doesn't}
+			a- Return Null
+	2- Iterate if bytes != 0 and the buffer are not an '\n'
+		- Every run save in the variable the fd read count,
+		   and check if the bytes readed > 0
+			{If its}
+				-Close the buffer and add the content to the string box
+			{If doesn't}
+				-Clean the strs memory
+
+[Description ft_cut]
+	1-Iterate while str exist and its not a \n
+		-Save counter (this save where is the \n)
+	2-Check if Str[cont] stills exist
+		{If doesn't}
+			a- free str and return NULL
+	3-Check if Str[cont] its equalt to \n
+		{If its}
+			a- iterate cont
+	4- Create and check malloc size of len_str - cont(+1 for \0)
+	5-Iterate while str[cont] exist and put the content in a temp str
+	6- Out of the iteration close the temp
+	7- Free the str and return temp
+
+
+*/
